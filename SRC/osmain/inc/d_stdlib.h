965c87b8a84235f41d3e431288f2d7d9c2f31836
Yoyo <98507772+Yo-yo-ooo@users.noreply.github.com>
2022-08-12 22:37:16 +0800

    d_stdlib更新

---
+++ SRC/osmain/inc/d_stdlib.h
@@ -0,0 +1,13 @@
+#ifndef _D_STDLIB_H
+#define _D_STDLIB_H
+
+/*
+* This File is for Skyline System stdlib Library
+* ----------------------------------------------
+* Object: Done For lib.h C stdlib.h Library
+*/
+int rand(void);
+void srand(unsigned int seed);
+
+
+#endif
