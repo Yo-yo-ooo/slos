242f6de36f38dafbe5f0b8ca1341ac5e1e078a45
Yoyo <98507772+Yo-yo-ooo@users.noreply.github.com>
2022-08-23 14:42:26 +0800

    oo

--- SRC/osmain/lib/cpp/iosfwd.cpp
+++ SRC/osmain/lib/cpp/iosfwd.cpp
@@ -1,39 +1,40 @@
 #include <inc/cpp/iosfwd>
+#include <inc/stdio.h>
 
 void ostream::operator << (char *msg)const{
     printf("%s",msg);
-    //return* this;
+     
 }
 
 void ostream::operator << (int value)const{
     printf("%d",value);
-    //return* this;
+     
 } 
 
 void ostream::operator << (float d)const{
     printf("%f",d);
-    //return* this;
+     
 }
 
 void ostream::operator << (double d)const{
     printf("%f",d);
-    //return* this;
+     
 }
 
 void istream::operator << (char *a)const{
     //帮我实现
-    //return*this;
+     
 }
 
 void istream::operator << (String str)const{
     //帮我实现
-    //return*this;
+     
 }
 
 void istream::operator >> (char *a)const{
-    //return*this;
+     
 }
 
 void istream::operator >> (String str)const{
-    //return*this;
+     
 }
