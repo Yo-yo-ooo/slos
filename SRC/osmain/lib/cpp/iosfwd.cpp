4dd45d6d426ef835c6280095e652f6a1e270966a
Yoyo <98507772+Yo-yo-ooo@users.noreply.github.com>
2022-08-23 14:03:42 +0800

    outstream

--- SRC/osmain/lib/cpp/iosfwd.cpp
+++ SRC/osmain/lib/cpp/iosfwd.cpp
@@ -1,5 +1,21 @@
 #include <inc/cpp/iosfwd>
 
-void ostream::operator << (char *msg){
+void outstream::operator << (char *msg)const{
     printf("%s",msg);
+    return* this;
+}
+
+void outstream::operator << (int value)const{
+    printf("%d",value);
+    return* this;
+} 
+
+void outstream::operator << (float d)const{
+    printf("%f",d);
+    return* this;
+}
+
+void outstream::operator << (double d)const{
+    printf("%f",d);
+    return* this;
 }
