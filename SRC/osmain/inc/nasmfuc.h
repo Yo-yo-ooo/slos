849b3b90c4f8a2bf1ec3334b106756d55b1634ab
Yoyo <98507772+Yo-yo-ooo@users.noreply.github.com>
2022-08-24 12:02:56 +0800

    ww

--- SRC/osmain/inc/nasmfuc.h
+++ SRC/osmain/inc/nasmfuc.h
@@ -4,17 +4,18 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-void _io_cli(void);
-void _io_sti(void);
-void _io_out8(int port, int data);
-void _io_out16(int port, int data);
-void _io_out32(int port, int data);
-int _io_in8(int port);
-int _io_in16(int port);
-int _io_in32(int port);
-int _io_load_eflags(void);
-void _io_store_eflags(int eflags);
-void _sleep(int time);
+void io_cli(void);
+void io_sti(void);
+void io_out8(int port, int data);
+void io_out16(int port, int data);
+void io_out32(int port, int data);
+int io_in8(int port);
+int io_in16(int port);
+int io_in32(int port);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+void sleep(int time);
+void io_hlt(void);
 
 #ifdef __cplusplus
 }
--- SRC/osmain/user/time.c
+++ SRC/osmain/user/time.c
@@ -4,16 +4,16 @@ void
 umain(int argc, char **argv){
     char c;
     char *time = "The current time is:00:00:00";
-	_io_out8(0x700000, 0);
-	c = _io_in8(0x710000);
+	io_out8(0x700000, 0);
+	c = io_in8(0x710000);
 	time[27] = (c & 0x0f) + 0x300000;
 	time[26] = (c >> 4) + 0x300000;
-	_io_out8(0x700000, 2);
-	c = _io_in8(0x710000);
+	io_out8(0x700000, 2);
+	c = io_in8(0x710000);
 	time[24] = (c & 0x0f0000) + 0x300000;
 	time[23] = (c >> 4) + 0x300000;
-	_io_out8(0x700000, 4);
-	c = _io_in8(0x710000);
+	io_out8(0x700000, 4);
+	c = io_in8(0x710000);
 	time[21] = (c & 0x0f) + 0x300000;
 	time[20] = (c >> 4) + 0x300000;
 	printf("%s",time);
