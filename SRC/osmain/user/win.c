0f9d593ac8242847039c4add4d6d8ba05a2040d2
Yoyo <98507772+Yo-yo-ooo@users.noreply.github.com>
2022-08-24 12:26:30 +0800

    a

--- SRC/osmain/user/win.c
+++ SRC/osmain/user/win.c
@@ -1,16 +1,16 @@
 #include <inc/sys/window.h>
+#include <inc/sys/io.h>
 #include <inc/nasmfuc.h>
+
+#define PIC1_DATA 0x21
+#define PIC2_DATA 0xA1
 
 void
 umain(int argc, char **argv){
-    int i; /*i为DWORD４个字节*/
-	char *p; /* p用于byte类型地址 */
+    outb(PIC1_DATA, 0b11111000);
+    outb(PIC2_DATA, 0b11101111);
 
-	p = (char *) 0xa0000; /*  */
-
-	for (i = 0; i <= 0xffff; i++) {
-		*(p + i) = i & 0x0f;				//这样对内存进行赋值以后就会显示出图案了．
-	}
+    asm("sti");
 
 	for (;;) {
 		asm ("hlt");
