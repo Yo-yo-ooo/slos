78e530e1ee3c11b24fec9f184fa95f47753d93b8
Yoyo <98507772+Yo-yo-ooo@users.noreply.github.com>
2022-08-25 12:41:44 +0800

    nasmfunction

--- SRC/osmain/user/time.c
+++ SRC/osmain/user/time.c
@@ -1,6 +1,6 @@
 #include <inc/lib.h>
 
-void io_cli(void);
+/* void io_cli(void);
 void io_sti(void);
 void io_out8(int port, int data);
 void io_out16(int port, int data);
@@ -11,11 +11,11 @@ int io_in32(int port);
 int io_load_eflags(void);
 void io_store_eflags(int eflags);
 void sleep(int time);
-void io_hlt(void);
+void io_hlt(void); */
 
 void
 umain(int argc, char **argv){
-    char c;
+    /* char c;
     char *time = "The current time is:00:00:00";
 	io_out8(0x700000, 0);
 	c = io_in8(0x710000);
@@ -29,7 +29,7 @@ umain(int argc, char **argv){
 	c = io_in8(0x710000);
 	time[21] = (c & 0x0f) + 0x300000;
 	time[20] = (c >> 4) + 0x300000;
-	printf("%s",time);
+	printf("%s",time); */
 	printf("\n\n");
 	return;
 
--- SRC/osmain/user/win.c
+++ SRC/osmain/user/win.c
@@ -1,85 +1,85 @@
 #include <inc/sys/window.h>
 #include <inc/sys/io.h>
 
-void io_cli(void);
-void io_sti(void);
-void io_out8(int port, int data);
-void io_out16(int port, int data);
-void io_out32(int port, int data);
-int io_in8(int port);
-int io_in16(int port);
-int io_in32(int port);
-int io_load_eflags(void);
-void io_store_eflags(int eflags);
-void sleep(int time);
-void io_hlt(void);
+// void io_cli(void);
+// void io_sti(void);
+// void io_out8(int port, int data);
+// void io_out16(int port, int data);
+// void io_out32(int port, int data);
+// int io_in8(int port);
+// int io_in16(int port);
+// int io_in32(int port);
+// int io_load_eflags(void);
+// void io_store_eflags(int eflags);
+// void sleep(int time);
+// void io_hlt(void);
 
-#define COL8_000000 0
-#define COL8_FF0000 1
-#define COL8_00FF00 2
-#define COL8_FFFF00 3
-#define COL8_0000FF 4
-#define COL8_FF00FF 5
-#define COL8_00FFFF 6
-#define COL8_FFFFFF 7
-#define COL8_C6C6C6 8
-#define COL8_840000 9
-#define COL8_008400 10
-#define COL8_848400 11
-#define COL8_000084 12
-#define COL8_840084 13
-#define COL8_008484 14
-#define COL8_848484 15
+// #define COL8_000000 0
+// #define COL8_FF0000 1
+// #define COL8_00FF00 2
+// #define COL8_FFFF00 3
+// #define COL8_0000FF 4
+// #define COL8_FF00FF 5
+// #define COL8_00FFFF 6
+// #define COL8_FFFFFF 7
+// #define COL8_C6C6C6 8
+// #define COL8_840000 9
+// #define COL8_008400 10
+// #define COL8_848400 11
+// #define COL8_000084 12
+// #define COL8_840084 13
+// #define COL8_008484 14
+// #define COL8_848484 15
 
-void set_palette(int start, int end, unsigned char *rgb) {
-  int eflags = io_load_eflags(); // 记录标志
+// void set_palette(int start, int end, unsigned char *rgb) {
+//   int eflags = io_load_eflags(); // 记录标志
 
-  io_cli(); // 禁止中断
+//   io_cli(); // 禁止中断
 
-  io_out8(0x03c8, start);
-  for (int i = start; i <= end; i++) {
-    io_out8(0x03c9, rgb[0] / 4);
-    io_out8(0x03c9, rgb[1] / 4);
-    io_out8(0x03c9, rgb[2] / 4);
-    rgb += 3;
-  }
+//   io_out8(0x03c8, start);
+//   for (int i = start; i <= end; i++) {
+//     io_out8(0x03c9, rgb[0] / 4);
+//     io_out8(0x03c9, rgb[1] / 4);
+//     io_out8(0x03c9, rgb[2] / 4);
+//     rgb += 3;
+//   }
 
-  io_store_eflags(eflags);
+//   io_store_eflags(eflags);
 
-  return;
-}
+//   return;
+// }
 
-void init_palette(void) {
-  static unsigned char table_rgb[16 * 3] = {
-      0x00, 0x00, 0x00, // 黑色
-      0xff, 0x00, 0x00, // 亮红色
-      0x00, 0xff, 0x00, // 亮绿色
-      0xff, 0xff, 0x00, // 亮黄色
-      0x00, 0x00, 0xff, // 亮蓝色
-      0xff, 0x00, 0xff, // 亮紫色
-      0x00, 0xff, 0xff, // 浅亮蓝色
-      0xff, 0xff, 0xff, // 白色
-      0xc6, 0xc6, 0xc6, // 亮灰色
-      0x84, 0x00, 0x00, // 暗红色
-      0x00, 0x84, 0x00, // 暗绿色
-      0x84, 0x84, 0x00, // 暗黄色
-      0x00, 0x00, 0x84, // 暗蓝色
-      0x84, 0x00, 0x84, // 暗紫色
-      0x00, 0x84, 0x84, // 浅暗蓝色
-      0x84, 0x84, 0x84  // 暗灰色
-  };
+// void init_palette(void) {
+//   static unsigned char table_rgb[16 * 3] = {
+//       0x00, 0x00, 0x00, // 黑色
+//       0xff, 0x00, 0x00, // 亮红色
+//       0x00, 0xff, 0x00, // 亮绿色
+//       0xff, 0xff, 0x00, // 亮黄色
+//       0x00, 0x00, 0xff, // 亮蓝色
+//       0xff, 0x00, 0xff, // 亮紫色
+//       0x00, 0xff, 0xff, // 浅亮蓝色
+//       0xff, 0xff, 0xff, // 白色
+//       0xc6, 0xc6, 0xc6, // 亮灰色
+//       0x84, 0x00, 0x00, // 暗红色
+//       0x00, 0x84, 0x00, // 暗绿色
+//       0x84, 0x84, 0x00, // 暗黄色
+//       0x00, 0x00, 0x84, // 暗蓝色
+//       0x84, 0x00, 0x84, // 暗紫色
+//       0x00, 0x84, 0x84, // 浅暗蓝色
+//       0x84, 0x84, 0x84  // 暗灰色
+//   };
 
-  set_palette(0, 15, table_rgb);
+//   set_palette(0, 15, table_rgb);
 
-  return;
-}
+//   return;
+// }
 
 void
 umain(int argc, char **argv){
     unsigned char *vram = (unsigned char *)0xa0000;
     int xSize = 320, ySize = 200;
 
-    init_palette();
+    /* init_palette();
 
     boxfill8(vram, xSize, COL8_008484, 0, 0, xSize - 1, ySize - 29);
     boxfill8(vram, xSize, COL8_C6C6C6, 0, ySize - 28, xSize - 1, ySize - 28);
@@ -103,6 +103,6 @@ umain(int argc, char **argv){
                 ySize - 3);
     for (;;) {
         io_hlt();
-    }
+    } */
 
 }
