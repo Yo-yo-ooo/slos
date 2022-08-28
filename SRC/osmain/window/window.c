2bcbeb0063b10a9b74801df3fecc530dbec90d49
Yoyo <98507772+Yo-yo-ooo@users.noreply.github.com>
2022-08-28 12:30:37 +0800

    w

--- SRC/osmain/window/window.c
+++ SRC/osmain/window/window.c
@@ -5,7 +5,14 @@
 void
 umain(int argc, char **argv)
 {
+    int i;
+    char *p;
     
+    p = (char *) 0xa0000;
+
+    for(i = 0;i <= 0xffff;i++){
+        *(p + i) = i & 0x0f;
+    }
 
     for(;;){
         io_hlt();
