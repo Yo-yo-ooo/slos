e2677118cbc668b30c2a0f5540f9aab12d45fcf1
Yoyo <98507772+Yo-yo-ooo@users.noreply.github.com>
2022-10-09 11:02:23 +0800

    afxxx

--- SRC/osmain/inc/limits.h
+++ SRC/osmain/inc/limits.h
@@ -23,5 +23,24 @@
 #define LONG_MAX 2147483647
 #define ULONG_MIN 0
 #define ULONG_MAX 4294967295
+#define LLONG_MAX     9223372036854775807i64
+#define LLONG_MIN   (-9223372036854775807i64 - 1)
+#define ULLONG_MAX    0xffffffffffffffffui64
+
+#define _I8_MIN     (-127i8 - 1)
+#define _I8_MAX       127i8
+#define _UI8_MAX      0xffui8
+
+#define _I16_MIN    (-32767i16 - 1)
+#define _I16_MAX      32767i16
+#define _UI16_MAX     0xffffui16
+
+#define _I32_MIN    (-2147483647i32 - 1)
+#define _I32_MAX      2147483647i32
+#define _UI32_MAX     0xffffffffui32
+
+#define _I64_MIN    (-9223372036854775807i64 - 1)
+#define _I64_MAX      9223372036854775807i64
+#define _UI64_MAX     0xffffffffffffffffui64
 
 #endif
---
+++ SRC/osmain/inc/stdlib.h
@@ -0,0 +1,6 @@
+#ifndef _STDLIB_H
+#define _STDLIB_H
+
+#include <inc/d_stdlib.h>
+
+#endif
